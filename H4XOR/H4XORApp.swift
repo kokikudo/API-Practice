be80c33cd2c863f56872b23b0ccccd64ba460d81
kokikudo <massanmesu@icloud.com>
2022-02-01 10:31:46 +0900

    Initial Commit

---
+++ H4XOR/H4XORApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORApp.swift
+//  H4XOR
+//
+//  Created by kudo koki on 2022/02/01.
+//
+
+import SwiftUI
+
+@main
+struct H4XORApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
